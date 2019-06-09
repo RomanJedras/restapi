3426d2911da2ebdb756f6ac34876e67967ddc176
RomanJedras <romanjedras@o2.pl>
2019-06-09 21:40:09 +0200

    Remove unused data

--- config/configs.php
+++ config/configs.php
@@ -3,7 +3,7 @@
 
  $mysql = array(
 			"address" => 'localhost',
-			"user" => '30203458_clearcode',
-			"password" => 'f31c1b9a6256?-',
-			"database" => '30203458_clearcode'
+			"user" => '',
+			"password" => '',
+			"database" => ''
 		);
